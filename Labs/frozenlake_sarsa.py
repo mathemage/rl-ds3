8522163e1de6c2fb38d5e43814ea5bede9a262cc
Karel Ha <mathemage@gmail.com>
2018-06-28 16:45:44 +0200

    Implement SARSA
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- Labs/frozenlake_sarsa.py
+++ Labs/frozenlake_sarsa.py
@@ -2,18 +2,19 @@ import gym.spaces
 import numpy as np
 
 SEED = 1337
-N_TOTAL = 10
+EPISODES = 3
+# EPISODES = 3000
+GAMMA = 0.99
 
 env = gym.make('FrozenLake-v0')
 env.seed(SEED)
-observation = env.reset()
 n_a = env.action_space.n
 n_s = env.observation_space.n
-gamma = 0.99
 
 
 def init_q():
-	return np.zeros((n_s, n_a))
+	# return np.zeros((n_s, n_a))
+	return np.random.rand(n_s, n_a)
 
 
 def run_episodes(episodes=20):
@@ -34,29 +35,42 @@ def state_choice():
 	return 42
 
 
-def action_choice(q, s, a, epsilon=.5):
+def action_choice(q, s, epsilon=.5):
 	# TODO epsilon-greedy
-	print(q, s, a, epsilon)
-	return 42
+	a = np.argmax(q[s][:])
+	print(q[s][:])
+	print("a: {}".format(a))
+	return a
 
 
-def sarsa(alpha=0.1):
+def sarsa(environment, alpha=0.1):
+	"""
+	https://www.cse.unsw.edu.au/~cs9417ml/RL1/algorithms.html
+
+	:param environment: OpenAI Gym environment
+	:param alpha: learning rate for TD Q error
+	:return: the converged Q-table
+	"""
 	q = init_q()
-	for _ in range(N_TOTAL):
-		s = state_choice()
-		a = action_choice(q, s, a)
-		s_new, r, done, _ = env.step(a)
+	for ep in range(EPISODES):
+		print("Episode {}:".format(ep))
+		s = environment.reset()
+		a = action_choice(q, s)
+		done = False
 		while not done:
-			a_new = action_choice(q, s_new, a)
-			delta = r + gamma * q[s_new, a_new] - q[s, a]
+			s_new, r, done, _ = environment.step(a)
+			a_new = action_choice(q, s_new)
+			delta = r + GAMMA * q[s_new, a_new] - q[s, a]
 			q[s, a] += alpha * delta
 			s, a = s_new, a_new
+			print("delta: {}".format(delta))
+			print("Q-table:\n{}".format(q))
 	return q
 
 
 if __name__ == '__main__':
-	q_table = sarsa()
+	q_table = sarsa(environment=env)
 	# run_episodes()
 	print(n_a)
 	print(n_s)
-	print(q_table)
+	# print(q_table)
