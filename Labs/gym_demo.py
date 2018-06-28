343f2fcc48c17889ce10d1ab77a55e76a0d00eba
Karel Ha <mathemage@gmail.com>
2018-06-28 14:47:11 +0200

    Wrap in a for-loop
    - in `gym_demo.py`
    
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- Labs/gym_demo.py
+++ Labs/gym_demo.py
@@ -1,11 +1,21 @@
 import gym.spaces
 
+SEED = 1337
+STEPS = 5
+
 env = gym.make('FrozenLake-v0')
+env.seed(SEED)
 observation = env.reset()
 n_a = env.action_space.n
 n_s = env.observation_space.n
 
 print(n_a)
 print(n_s)
+
+for _ in range(STEPS):
+	env.render()
+	action = env.action_space.sample()
+	observation, reward, done, info = env.step(action)
+	print(action, observation, reward, done, info)
 
 env.render()
