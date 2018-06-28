89b7fde0daa785ea369973485031cef8c1f45edc
Karel Ha <mathemage@gmail.com>
2018-06-28 14:59:21 +0200

    Create `frozenlake_sarsa.py`
    Signed-off-by: Karel Ha <mathemage@gmail.com>

---
+++ Labs/frozenlake_sarsa.py
@@ -0,0 +1,24 @@
+import gym.spaces
+
+SEED = 1337
+STEPS = 5
+
+env = gym.make('FrozenLake-v0')
+env.seed(SEED)
+observation = env.reset()
+n_a = env.action_space.n
+n_s = env.observation_space.n
+
+print(n_a)
+print(n_s)
+
+for i_episode in range(20):
+	observation = env.reset()
+	for t in range(100):
+		env.render()
+		print(observation)
+		action = env.action_space.sample()
+		observation, reward, done, info = env.step(action)
+		if done:
+			print("Episode finished after {} timesteps".format(t+1))
+			break
