7eff6471bcb78c63ae2b99451fc9c45c69a9565e
Karel Ha <mathemage@gmail.com>
2018-06-29 15:19:39 +0200

    Solve Ex 2
    Signed-off-by: Karel Ha <mathemage@gmail.com>

--- Labs/cartpole_pg.py
+++ Labs/cartpole_pg.py
@@ -11,7 +11,9 @@ LEFT = 0
 RIGHT = 1
 
 VERBOSE = True
-
+DEFAULT_THETA = [.5, .5, .5, .5]
+SEED = 1337
+np.random.seed(SEED)
 
 # Compute policy parameterisation
 def sigmoid(x):
@@ -25,16 +27,47 @@ def get_policy(s, theta):
 
 # Draw an action according to current policy
 def act_with_policy(s, theta):
-	pass
+	policy_threshold = get_policy(s, theta) > 1
+	if np.random.rand() < policy_threshold:
+		return 1
+	else:
+		return 0
 
 
 # Generate an episode
 def rollout(env, theta, max_episode_length=EPISODE_DURATION, render=False):
-	pass
+	episode_actions, episode_rewards = [], []
+	s = env.reset()
+	episode_states = [s]
+	for _ in range(max_episode_length):
+		if render:
+			env.render()
+		a = act_with_policy(s, theta)
+		s, r, done, _ = env.step(a)
+		episode_states.append(s)
+		episode_actions.append(a)
+		episode_rewards.append(r)
+		if done:
+			return episode_states, episode_actions, episode_rewards
 
 
 def test_policy(env, theta, score=SCORE, num_episodes=TEST_TIME, max_episode_length=EPISODE_DURATION, render=False):
-	pass
+	num_success = 0
+	average_return = 0
+	for i_episode in range(num_episodes):
+		_, _, episode_rewards = rollout(env, theta, max_episode_length, render)
+		total_rewards = sum(episode_rewards)
+		if total_rewards > score:
+			num_success += 1
+		average_return += (1.0 / num_episodes) * total_rewards
+		if render:
+			print(
+				"Test Episode {0}: Total Reward = {1} - Success = {2}".format(i_episode, total_rewards, total_rewards > score))
+	if average_return > score:
+		success = True
+	else:
+		success = False
+	return success, num_success, average_return
 
 
 # Returns Policy Gradient for a given episode
@@ -43,27 +76,35 @@ def compute_pg(episode_states, episode_actions, episode_rewards, theta):
 
 
 # Train until average return is larger than SCORE
-def train(env, theta_init, max_episode_length=EPISODE_DURATION, alpha_init=ALPHA_INIT):
-	theta, i, average_returns = 0, 0, []
+def train(env, theta_init=DEFAULT_THETA, max_episode_length=EPISODE_DURATION, alpha_init=ALPHA_INIT):
+	theta, i, average_returns = theta_init, 0, []
 	return theta, i, average_returns
 
 
 def main():
 	env = gym.make(ENV_NAME)
+	env.seed(SEED)
 	dim = env.observation_space.shape[0]
 	# Init parameters to random
-	theta_init = np.random.randn(1, dim)
+	# theta_init = np.random.randn(dim)
+	theta_init = [.5, .5, .5, .5]
+
 	# Train agent
 	theta, i, average_returns = train(env, theta_init)
 	print("Solved after {} iterations".format(i))
-	# Test final policy
-	test_policy(env, theta, num_episodes=10, render=True)
+
 	# Show training curve
 	plt.plot(range(len(average_returns)), average_returns)
 	plt.title("Average reward on 100 episodes")
 	plt.xlabel("Training Steps")
 	plt.ylabel("Reward")
 	plt.show()
+
+	# Test final policy
+	success, num_success, average_return = test_policy(env, theta, num_episodes=10, render=True)
+	print("success == {}".format(success))
+	print("num_success == {}".format(num_success))
+	print("average_return == {}".format(average_return))
 	env.close()
 
 
